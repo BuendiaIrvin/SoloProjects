99cee46694431b46d319241c8e17921b9aadbe73
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-14 13:58:50 -0500

    dynamic message edit

--- js/message.js
+++ js/message.js
@@ -2,10 +2,31 @@ const button = document.querySelector("button");
 const output = document.querySelector(".output");
 const myName = document.querySelector("input");
 
+
 console.log(button);
 
-button.addEventListener("click", showMessage);
+// button.addEventListener("click", showMessage);
+button.addEventListener("click", showOutput);
 
-function showMessage(){
-    output.innerHTML = `<h1>Welcome to your page ${myName.value}, enjoy.</h1>`
-}
+function showOutput() {
+    const date = new Date();
+    let cur = date.getHours();
+    let message;
+
+    if (cur > 17) {
+        message = "Good Evening";
+        output.style.backgroundColor = "green";
+    } else if (cur > 12) {
+        message = "Good Afternoon";
+        output.style.backgroundColor = "orange";
+    } else if (cur > 0) {
+        message = "Good Morning";
+        output.style.backgroundColor = "yellow";
+    } else {
+        message = "Something is Wrong";
+        output.style.backgroundColor = "red";
+    }
+    console.log(date.toDateString());
+
+        output.innerHTML = `<h1>${message} ${myName.value}, Welcome to your page.</h1>`
+    }
